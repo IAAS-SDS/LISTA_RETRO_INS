edd62589b50bbd63e607df74534589867d3ac0de
JLGG-DevSoft <JLGG_devsoft@outlook.com>
2026-05-25 09:05:11 -0500

    Marzo retros

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const CONFIG = {
   // Configura aqui hasta que fecha se puede editar cada hoja.
   // Formato: YYYY-MM-DD
   editDeadlines: {
-    ENERO_2026: "2026-05-20",
-    FEBRERO_2026: "2026-05-20",
-    MARZO_2026: "2026-04-15",
+    ENERO_2026: "2026-06-15",
+    FEBRERO_2026: "2026-06-15",
+    MARZO_2026: "2026-06-15",
     ABRIL_2026: "2026-05-15",
     MAYO_2026: "2026-06-15",
     JUNIO_2026: "2026-07-15",
